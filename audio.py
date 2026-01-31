3cc7306801c5f677eeee939532ebdbd3c4267047
leva <gilevlevevg@gmail.com>
2026-01-31 20:53:11 +0500

    Добавил рандомый звук ударов

--- audio.py
+++ audio.py
@@ -4,10 +4,13 @@
 Сейчас в проекте предпочтительный путь — работать со звуком через `spritePro.audio_manager`
 и события/системы. Этот файл оставлен для совместимости и примеров.
 """
-
+from random import choice
 import spritePro as s
 from game.paths import PATHS
-attack_sound:s.Sound = s.load_sound('attack',PATHS.audio +'/Blow1.ogg')
+def play_attack_sound():
+    list_random_hit_sound  = ['/Blow1.ogg','/Blow2.ogg','/Blow3.ogg',]
+    audio_hit = PATHS.audio + choice(list_random_hit_sound)
+    s.audio_manager.play_sound(audio_hit)
 
 def play_music_game():
     """Запустить фоновую музыку (если файл существует)."""
@@ -15,5 +18,5 @@ def play_music_game():
 
 def stop_music():
     """Остановить фоновую музыку."""
-    s.audio.stop_music()
+    s.audio_manager.stop_music()
 
